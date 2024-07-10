a7b26bfd0b455d6f4051e245df0047abb06d9ca8
Pedro Durand <webpedrod@gmail.com>
2024-07-10 16:39:56 -0300

    env config

--- src/api/subscription.ts
+++ src/api/subscription.ts
@@ -4,7 +4,7 @@ export async function getSubscriptionStatus(
   userEmail: string | null
 ): Promise<SubscriptionStatusResponse> {
   const resp = await fetch(
-    `${process.env.API_BASE_URL}/subscriptions/${userEmail}`
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/subscriptions/${userEmail}`
   );
 
   if (!resp.ok) {
